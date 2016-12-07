8c5d17d52ec40a7548da010fa4e402f815973987
Getro Naissance and Jim Padilla <getro@>
2016-12-07 13:57:27 -0800

    Add all, save, name specs and methods for Train

---
+++ lib/train.rb
@@ -0,0 +1,30 @@
+class Train
+
+attr_reader(:id, :name)
+
+  define_method(:initialize) do |attributes|
+    @name = attributes.fetch(:name)
+    @id = attributes.fetch(:id)
+  end
+
+  define_method(:save) do
+    result = DB.exec("INSERT INTO train (name) VALUES ('#{@name}') RETURNING id;")
+    @id = result.first().fetch('id').to_i
+  end
+
+  define_singleton_method(:all) do
+    returned_trains = DB.exec("SELECT * FROM train;")
+    all_trains = []
+    returned_trains.each do |train|
+      name = train.fetch('name')
+      id = train.fetch('id').to_i
+      all_trains.push(Train.new({:id => id, :name => name}))
+    end
+    all_trains
+  end
+
+  define_method(:==) do |other_train|
+    self.name() == other_train.name() && self.id() == other_train.id()
+  end
+
+end
--- spec/train_spec.rb
+++ spec/train_spec.rb
@@ -2,10 +2,33 @@ require('spec_helper')
 
 describe(Train) do
 
+  describe('.all') do
+    it('returns empty at first') do
+      expect(Train.all()).to(eq([]))
+    end
+  end
+
   describe('#id') do
     it('Returns id of the train') do
-      test_train = Train.new(:id => nil)
-      expect().to(eq())
+      test_train = Train.new(:id => nil, :name => 'Getro')
+      test_train.save()
+      expect(test_train.id()).to(be_an_instance_of(Fixnum))
+    end
+  end
+
+  describe('#name') do
+    it('Returns the name of the train') do
+      test_train = Train.new({:id => nil, :name => 'Getro'})
+      expect(test_train.name()).to(eq('Getro'))
+    end
+  end
+
+
+  describe('#save') do
+    it('returns the saved trains') do
+      test_train = Train.new({:id => nil, :name => 'James'})
+      test_train.save()
+      expect(Train.all()).to(eq([test_train]))
     end
   end
 
